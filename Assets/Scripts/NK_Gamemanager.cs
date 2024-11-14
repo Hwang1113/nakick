bafdb3df2c101bdbdbf39884eaa48ea53474ea20
HWANG <jhdsew9471@gmail.com>
2024-11-14 09:22:12 +0900

    기초틀공

---
+++ Assets/Scripts/NK_Gamemanager.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class NK_Gamemanager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject NK_Player = null;
+    [SerializeField]
+    private GameObject NK_Trash = null;
+}
