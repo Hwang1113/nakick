d85de775b406ff0f18f83cb1db1e58e0e8a0be31
DESKTOP-P1OUMPK\admin <dndb0413@naver.com>
2024-11-14 11:35:09 +0900

    스크립트

--- Assets/Scripts/TrashManager.cs
+++ Assets/Scripts/TrashManager.cs
@@ -2,20 +2,33 @@ using UnityEngine;
 
 public class TrashManager : MonoBehaviour
 {
+    public GameObject rangeObject;
+    BoxCollider rangeCollider;
     public GameObject Item;
+    bool isGet 
+
 
     private void Start()
     {
-        Invoke("Spawnitem", 30);
+        GameObject istantItem = Instantiate(Item, Return_RandomPosition(), Quaternion.identity);
+    }
+    private void Awake()
+    {
+        rangeCollider = rangeObject.GetComponent<BoxCollider>();
+
     }
 
-    private void Spawnitem()
+    Vector3 Return_RandomPosition()
     {
-        float randomX = Random.Range(-23f, 19f);
-        float randomZ = Random.Range(-23f, 19f);
-        if(true)
-        {
-            GameObject item = (GameObject)Instantiate(Item, new Vector3(randomX, 0f, randomZ), Quaternion.identity);
-        }
+        Vector3 originPosition = rangeObject.transform.position;
+        float range_X = rangeCollider.bounds.size.x;
+        float range_Z = rangeCollider.bounds.size.z;
+
+        range_X = Random.Range((range_X / 2) * -1, range_X / 2);
+        range_Z = Random.Range((range_Z / 2) * -1, range_Z / 2);
+        Vector3 RandomPosition = new Vector3(range_X, 0.5f, range_Z);
+
+        Vector3 respawnPosition = originPosition + RandomPosition;
+        return respawnPosition;
     }
 }
