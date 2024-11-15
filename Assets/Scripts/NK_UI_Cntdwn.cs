17bb74d58d0ad73041d02de8d865d99b2286049f
DESKTOP-P1OUMPK\admin <dndb0413@naver.com>
2024-11-15 16:27:24 +0900

    게임 매니저 스크립

--- Assets/Scripts/NK_UI_Cntdwn.cs
+++ Assets/Scripts/NK_UI_Cntdwn.cs
@@ -2,21 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class NK_UI_Cntdwn : MonoBehaviour
 {
+    //public static NK_UI_Cntdwn Instance;
+
     [SerializeField]
     private TextMeshProUGUI TM_Pro = null;
     public const float maxCnt = 6f; // 카운트 다운 최대시간
     public float curCnt = 0f; //현재시간 담을 변수
     public string strCnt = null; // 카운트 문자열로 반환후 보내기 위한 빈 문자열
     public float playTime = 0;
-    public const float maxPlayTime = 30f;
+    public const float maxPlayTime = 10f;
+
+
+    private void Awake()
+    {
+        //if (Instance == null)
+        //{
+        //    Instance = this;
+        //}
+        TM_Pro = transform.GetComponent<TextMeshProUGUI>();
+        TM_Pro.enabled = false;
+    }
 
     private void Start()
     {
-        TM_Pro = transform.GetComponent<TextMeshProUGUI>();
-        TM_Pro.enabled = false;
+        
+
+        
     }
     //private void Update()
     //{
@@ -44,16 +59,8 @@ public class NK_UI_Cntdwn : MonoBehaviour
 
     private void Update()
     {
-        CountDownStart();
-        if (curCnt == 0f)
-        {
-            Play30();
-            if (curCnt == 0f)
-            {
-                CountDownEnd();
-                GameManager.instance.EndGame();
-            }
-        }
+        //CountDownStart();
+        
     }
 
     public void CountDownStart()//5초 카운트 다운하는 퍼블릭 함수 (정확히는 실수 6에서델타타임을 뺀 정수 )5부터 ~ 1까지는 정수 숫자가 나오고 1~0은 Start!
@@ -74,6 +81,7 @@ public class NK_UI_Cntdwn : MonoBehaviour
             TM_Pro.text = (strCnt);
             if(curCnt <1)
                 TM_Pro.text = "Start!";
+            Debug.Log("curCnt: "+ curCnt);
             yield return null;
         }
     }
@@ -91,13 +99,17 @@ public class NK_UI_Cntdwn : MonoBehaviour
 
     IEnumerator CountdownEnding5() 
     {
+        
         while (curCnt >=0)
         {
             curCnt -= Time.deltaTime; //6에서 매번 델타타임을 뺌
             strCnt = ((int)curCnt).ToString(); // 문자열 cnt에 curCnt를 정수형으로 바꿔서 넣음 
             TM_Pro.text = ("Go to lobby on\n" + strCnt);
-            if(curCnt<0)
+            if (curCnt < 0)
+            {
                 TM_Pro.text = null; // 문자열 초기화
+                SceneManager.LoadScene("B.Lobby");
+            }
             yield return null;
         }
     }
