d634d0dbd4a79961999ab6f26bb01482c6c8303a
ljhub-git <ljhunity@gmail.com>
2024-11-15 14:16:25 +0900

    플레이어 쓰레기 메서드명 변경
    플레이어 쓰레기 메서드명 변경

--- Assets/Scripts/NK_Trash.cs
+++ Assets/Scripts/NK_Trash.cs
@@ -3,10 +3,10 @@ using UnityEngine;
 public class NK_Trash : MonoBehaviour
 {
     private int touchCount = 0; //터치횟수 받기
-    private const int requiredtouchs = 3; //총 필요 터치 횟수 
+    private const int requiredtouchs = 3; //총 필요 터치 횟수(hp)
 
-    public delegate void TrashClickedEvent(Player player);
-    public static event TrashClickedEvent OnTrashtouched; // 클릭 이벤트
+    public delegate void TrashTouchEvent(Player player);
+    public static event TrashTouchEvent OnTrashtouched; // 터치 이벤트
 
     // 플레이어가 클릭(터치)할 때마다 호출되는 메서드
     public void PlayerTouch(Player player)
@@ -23,19 +23,19 @@ public class NK_Trash : MonoBehaviour
         }
     }
 
-    // 클릭 이벤트에 대한 리스너 등록 및 해제
+    // 터치 이벤트에 대한 리스너 등록 및 해제
     private void OnEnable()
     {
-        OnTrashtouched += HandleTrashClicked;
+        OnTrashtouched += HandleTrashTouched;
     }
 
     private void OnDisable()
     {
-        OnTrashtouched -= HandleTrashClicked;
+        OnTrashtouched -= HandleTrashTouched;
     }
 
     // Trash 클릭 완료 후 처리 (스코어 증가 등)
-    private void HandleTrashClicked(Player player)
+    private void HandleTrashTouched(Player player)
     {
         Debug.Log($"{player.name} has completed 10 clicks and earned a point!");
         player.AddScore(1); // 클릭을 완료한 플레이어에게 1점 추가
