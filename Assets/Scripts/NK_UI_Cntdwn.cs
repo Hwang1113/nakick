00bbdbaa263bdb1ce951ac8bc0faaecc92578df9
HWANG <jhdsew9471@gmail.com>
2024-11-15 12:05:34 +0900

    카운트다운 다 완성

--- Assets/Scripts/NK_UI_Cntdwn.cs
+++ Assets/Scripts/NK_UI_Cntdwn.cs
@@ -7,9 +7,12 @@ public class NK_UI_Cntdwn : MonoBehaviour
 {
     [SerializeField]
     private TextMeshProUGUI TM_Pro = null;
-    public const float maxCnt = 6f;
-    public float curCnt = 0f;
-    public string strCnt = null;
+    public const float maxCnt = 6f; // 카운트 다운 최대시간
+    public float curCnt = 0f; //현재시간 담을 변수
+    public string strCnt = null; // 카운트 문자열로 반환후 보내기 위한 빈 문자열
+    public float playTime = 0;
+    public const float maxPlayTime = 30f;
+
     private void Start()
     {
         TM_Pro = transform.GetComponent<TextMeshProUGUI>();
@@ -28,6 +31,10 @@ public class NK_UI_Cntdwn : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Z))    //디버깅용 업데이트 Z키 누르면 카운트 다운 일시정지
         {
             CountStop();
+        }       
+        if (Input.GetKeyDown(KeyCode.Q))    //디버깅용 업데이트 Q키 누르면 플레이 시간 30초 카운트 다운
+        {
+            Play30();
         }
     }
 
@@ -35,10 +42,9 @@ public class NK_UI_Cntdwn : MonoBehaviour
 
     public void CountDownStart()//5초 카운트 다운하는 퍼블릭 함수 (정확히는 실수 6에서델타타임을 뺀 정수 )5부터 ~ 1까지는 정수 숫자가 나오고 1~0은 Start!
     {
+        CountStop();
         TM_Pro.enabled = true;
-        StopCoroutine("CountdownEnd5");
-        StopCoroutine("CountdownStart5");
-        curCnt = maxCnt; // 카운트 초 초기화 여기서
+        curCnt = (int)maxCnt; // 카운트 초 초기화 여기서
         TM_Pro.text = null; // 문자열 초기화
         StartCoroutine("CountdownStart5");
     }
@@ -60,9 +66,8 @@ public class NK_UI_Cntdwn : MonoBehaviour
 
     public void CountDownEnd()//("Go to lobby on\n"+5)초 카운트 다운하는 퍼블릭 함수 (정확히는 실수 6에서델타타임을 뺀 정수 )5부터 ~ 0까지 
     {
+        CountStop();
         TM_Pro.enabled = true;
-        StopCoroutine("CountdownStart5");
-        StopCoroutine("CountdownEnding5");
         curCnt = maxCnt; // 카운트 초 초기화 여기서
         TM_Pro.text = null; // 문자열 초기화
         StartCoroutine("CountdownEnding5");
@@ -80,12 +85,35 @@ public class NK_UI_Cntdwn : MonoBehaviour
             yield return null;
         }
     }
-        
+    //=================================    
+    public void Play30()
+    {
+        CountStop();
+        TM_Pro.enabled = true;
+        curCnt = maxPlayTime; // 카운트 초 초기화 여기서
+        TM_Pro.text = null; // 문자열 초기화
+        StartCoroutine("Countdown30");
+    }
+    IEnumerator Countdown30()
+    {
+        while (curCnt >= 0)
+        {
+            curCnt -= Time.deltaTime; //6에서 매번 델타타임을 뺌
+            strCnt = curCnt.ToString("N2"); // 문자열 cnt에 curCnt를 정수형으로 바꿔서 넣음 
+            TM_Pro.text = (strCnt);
+            if (curCnt < 0)
+                TM_Pro.text = null; // 문자열 초기화
+            yield return null;
+        }
+    }
+    //=================================
+
+
     public void CountStop() //일시정지만 카운트는 초기화 되지 않음, 카운트 다운 UI 안보이게
     {
         TM_Pro.enabled = false;
         StopCoroutine("CountdownStart5");
-        StopCoroutine("CountdownEnding5");
+        StopCoroutine("CountdownEnding5"); 
+        StopCoroutine("Countdown30");
     }
-
 }
